0e300ca16e77afa37349029bd387068413a426c3
Abhijit287 <khandaabhijit287@gmail.com>
2025-11-04 07:54:39 +0530

    Add files via upload

---
+++ Hotel-booking/HotelServer/src/main/java/com/example/hotel/model/User.java
@@ -0,0 +1,66 @@
+package com.example.hotel.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+    
+    // REMOVE THESE MANUAL CONSTRUCTORS:
+    // public User() {}
+    // public User(String username, String password, String fullName) {
+    //     this.username = username;
+    //     this.password = password;
+    //     this.fullName = fullName;
+    // }
+}
